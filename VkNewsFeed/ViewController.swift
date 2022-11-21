661159b7868b7c1bdb25ab4a99c45a08b25295c5
Shibaeva Svetlana <68277146+svetlanashibaeva@users.noreply.github.com>
2022-11-21 15:42:22 +0500

    Initial Commit

---
+++ VkNewsFeed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VkNewsFeed
+//
+//  Created by Света Шибаева on 21.11.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
